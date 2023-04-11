64f72c7997bb7adf46d3a35c04f54427bb5959d7
Oscar <oscarantonio.soto1998@gmail.com>
2023-04-11 12:37:57 -0400

    Fix: consult categories by api

---
+++ pages/api/categories.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client"
+
+
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  const categories = await prisma.category.findMany();
+
+  res.status(200).json(categories)
+}
