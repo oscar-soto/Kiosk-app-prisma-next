2fe39d7f3d076701d62beace87f6b36b0f73d6d2
Oscar <oscarantonio.soto1998@gmail.com>
2023-04-11 12:34:47 -0400

    Fix: consult categories

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { PrismaClient } from '@prisma/client';
 
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-export default function Home() {
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home({ categories }) {
+  console.log(categories);
   return (
     <>
       <h1>Next JS</h1>
     </>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categories = await prisma.category.findMany();
+
+  return {
+    props: {
+      categories,
+    },
+  };
+};
