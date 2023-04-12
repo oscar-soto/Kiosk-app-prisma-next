ccface21ac5630779344e9333ec6d593146a9ed4
Oscar <oscarantonio.soto1998@gmail.com>
2023-04-11 23:52:06 -0400

    Fix: show current category on index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import useKiosk from '@/hooks/useKiosk';
 import Layout from '@/layout/Layout';
 import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
+  const { currentCategory } = useKiosk();
   return (
     <>
-      <Layout>
-        <h1>Inicio</h1>
+      <Layout page={`Menú ${currentCategory?.name || 'comida' }`}>
+        <h1 className='text-4xl font-black'>{currentCategory?.name}</h1>
+        <p className='text-2xl my-10'>
+          Elige y personaliza tu pedido
+        </p>
       </Layout>
     </>
   );
