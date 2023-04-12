2bf9749a20d037d204f4060d376372a9b79f6b63
Oscar <oscarantonio.soto1998@gmail.com>
2023-04-12 18:04:20 -0400

    Fix: select amount to the order

--- components/ModalProduct.js
+++ components/ModalProduct.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import Image from 'next/image';
 
 import useKiosk from '@/hooks/useKiosk';
 import { formatMoney } from '@/helpers';
 
 export const ModalProduct = () => {
-  const { product,handleChangeModal } = useKiosk();
+  const [amount, setAmount] = useState(1);
+  const { product, handleChangeModal } = useKiosk();
 
   return (
     <div className="md:flex gap-10">
@@ -18,7 +20,7 @@ export const ModalProduct = () => {
       </div>
 
       <div className="md:w-2/3">
-        <div className='flex justify-end'>
+        <div className="flex justify-end">
           <button onClick={handleChangeModal}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -41,6 +43,56 @@ export const ModalProduct = () => {
         <p className="mt-5 font-black text-5xl text-amber-500">
           {formatMoney(product.price)}
         </p>
+
+        <div className="flex gap-4 mt-5">
+          <button
+            type="button"
+            onClick={() => {
+              if (amount <= 1) return;
+              setAmount(amount - 1);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+
+          <p className="text-3xl">{amount}</p>
+
+          <button
+            type="button"
+            onClick={() => {
+              if (amount >= 5) return;
+              setAmount(amount + 1);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
