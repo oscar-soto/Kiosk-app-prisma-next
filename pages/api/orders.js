8283347a7440bf2973da2eadc05d9a67ab15bce5
Oscar <oscarantonio.soto1998@gmail.com>
2023-04-18 22:27:38 -0400

    Fix: add order into BD

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,8 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
 export default async function handler(req, res) {
-  if(req.method === 'POST') {
-    console.log(req.body)
-    res.json({metodo: "POST!!"})
-  } else {
-    res.json({metodo: 'GET!!!'});
+  const prisma = new PrismaClient();
+
+  if (req.method === 'POST') {
+    const order = await prisma.order.create({
+      data: {
+        name: req.body.name,
+        total: req.body.total,
+        order: req.body.order,
+        date: req.body.date,
+      },
+    });
+
+    res.json(order);
   }
 }
